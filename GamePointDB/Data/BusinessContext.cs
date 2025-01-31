cfd234e9bcb86fa65e474199593ec5fa5aa5904a
MiguelMatos1572 <miguelmatos1572@gmail.com>
2025-01-31 18:46:31 +0000

    DataBase

---
+++ GamePointDB/Data/BusinessContext.cs
@@ -0,0 +1,21 @@
+﻿using GamePointDB.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePointDB.Data
+{
+    internal class BusinessContext : DbContext, IBusinessContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=SQL6033.site4now.net;Initial Catalog=db_ab245f_miguelmatos001;User Id=db_ab245f_miguelmatos001_admin;Password=G4m3p01nt");
+        }
+    }
+
+}
---
+++ GamePointDB/Data/IBusinessContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GamePointDB.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamePointDB.Data
+{
+    public interface IBusinessContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
