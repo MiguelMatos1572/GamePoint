1229a12e1d149b59948a82ab1de925fa13f78108
Trolllucas <lucas.lucasmendes@gmail.com>
2025-01-31 17:59:22 +0000

    meti referencia a gettasks mas podesse mudar e o builder para fazer referencia ao IWebAPI

---
+++ GamePoint/IWebApi.cs
@@ -0,0 +1,11 @@
+﻿using GamePoint.WebAPI.Models;
+using Refit;
+
+namespace GamePoint.WebAplication
+{
+    public interface IWebApi
+    {
+        [Get("/gettasks")]
+        Task<List<GamePointModel>> GetGamePoints();
+    }
+}
