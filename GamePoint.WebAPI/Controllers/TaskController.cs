a7784c7a5dc7168d697f03cdc10003eaec10e9fa
Trolllucas <lucas.lucasmendes@gmail.com>
2025-02-05 19:35:16 +0000

    não sei porque o task controller não aceita GamePointDB como referencia

---
+++ GamePoint.WebAPI/Controllers/TaskController.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using GamePointDB.Data;
+using GamePointDB.Entity;
+using GamePoint.WebAPI.Models;
+
+
+namespace GamePoint.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        
+    }
+}
